66220671c8ffef58a8f43c76659b010d0a40befc
lucasporrini <2608lucas@gmail.com>
2024-10-08 16:52:52 +0200

    feature(core): added profile page

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -18,7 +18,10 @@ const ProfilePage = () => {
           src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671122.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1728172800&semt=ais_hybrid"
           alt="User avatar"
         />
-        <h1>{user?.username}</h1>
+        <h3 className="text-lg">
+          {user?.username.charAt(0).toUpperCase()}
+          {user?.username.slice(1)}
+        </h3>
       </div>
       <Button
         onClick={() => {
